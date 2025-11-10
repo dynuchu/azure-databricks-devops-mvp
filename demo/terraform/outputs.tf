f1f8b17f711234ed30bcad11d382f1ff974fb68b
Agnes <dynuchu@gmail.com>
2025-11-10 16:04:11 +0100

    add ai services

--- demo/terraform/outputs.tf
+++ demo/terraform/outputs.tf
@@ -1,2 +1,9 @@
-output "databricks_url" { value = azurerm_databricks_workspace.dbw.workspace_url }
-output "adf_name" { value = azurerm_data_factory.adf.name }
+output "databricks_url" {
+  value = azurerm_databricks_workspace.dbw.workspace_url
+}
+output "adf_name" {
+  value = azurerm_data_factory.adf.name
+}
+output "ai_endpoint" {
+  value = azurerm_cognitive_account.ai.endpoint
+}
